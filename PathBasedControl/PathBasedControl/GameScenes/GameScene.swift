f184be0aa6a66e129e73f647116fd568bb35fd08
KasRoid <KasRoid@gmail.com>
2022-01-07 16:47:38 +0900

    UI: Path Control with Multiple Objects

--- PathBasedControl/PathBasedControl/GameScenes/GameScene.swift
+++ PathBasedControl/PathBasedControl/GameScenes/GameScene.swift
@@ -13,26 +13,64 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         anchorPoint = CGPoint(x: 0, y: 0)
         backgroundColor = .black
-        let spriteNode = SKSpriteNode(imageNamed: "skull")
-        spriteNode.position = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
-        addChild(spriteNode)
-        
+        addSkulls(view: view)
+        createLine()
+    }
+}
+
+// MARK: - UI
+extension GameScene {
+    private func drawSnakeShapePath() -> CGMutablePath {
         let path = CGMutablePath()
-        path.move(to: CGPoint(x: 0, y: 40))
-        path.addLine(to: CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2))
-        path.addLine(to: CGPoint(x: view.bounds.width, y: 40))
-        path.closeSubpath()
+        let xOffset: CGFloat = 40
+        let widthEndPoint = (view?.bounds.width ?? 0) - xOffset
+        let height = view?.bounds.height ?? 0
+        var yOffset: CGFloat = -20
         
-        let followAction = SKAction.follow(path, asOffset: false, orientToPath: true, duration: 3)
-        let reverseAction = followAction.reversed()
-        let sequenceAction = SKAction.sequence([followAction, reverseAction])
-        let repeatAction = SKAction.repeatForever(sequenceAction)
-        spriteNode.run(repeatAction)
+        path.move(to: CGPoint(x: xOffset, y: height + yOffset))
+        let numberOfLines = 4
+        let finalStep = 4
         
+        for _ in 1...numberOfLines {
+            for currentStep in 1...finalStep {
+                let x = currentStep == 1 || currentStep == 2 ? widthEndPoint : xOffset
+                path.addLine(to: CGPoint(x: x, y: height + yOffset))
+                if currentStep % 2 != 0 {
+                    yOffset -= 30
+                }
+            }
+        }
+        return path
+    }
+    
+    private func addSkulls(view: SKView) {
+        let addSkullAction = SKAction.run { self.addNode(view: view, imageName: "skull", action: self.addFollowAction) }
+        let waitAction = SKAction.wait(forDuration: 0.2)
+        let sequenceAction = SKAction.sequence([addSkullAction, waitAction])
+        let repeatAction = SKAction.repeat(sequenceAction, count: 20)
+        run(repeatAction)
+    }
+    
+    private func addNode(view: SKView, imageName: String, action: ((SKSpriteNode) -> Void)?) {
+        let spriteNode = SKSpriteNode(imageNamed: imageName)
+        spriteNode.position = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
+        action?(spriteNode)
+        addChild(spriteNode)
+    }
+    
+    private func createLine() {
         let line = SKShapeNode()
-        line.path = path
+        line.path = drawSnakeShapePath()
         line.lineWidth = 1
         line.strokeColor = .red
         addChild(line)
+    }
+}
+
+// MARK: - Actions
+extension GameScene {
+    private func addFollowAction(_ node: SKSpriteNode) {
+        let followAction = SKAction.follow(drawSnakeShapePath(), asOffset: false, orientToPath: false, duration: 20)
+        node.run(followAction)
     }
 }
