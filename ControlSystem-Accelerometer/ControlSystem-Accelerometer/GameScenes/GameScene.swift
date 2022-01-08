d0f7e82b7b3b6be9983e6542dad8dc1b952224de
KasRoid <KasRoid@gmail.com>
2022-01-09 08:36:21 +0900

    feat: Implemented Y axis Accelerometer

--- ControlSystem-Accelerometer/ControlSystem-Accelerometer/GameScenes/GameScene.swift
+++ ControlSystem-Accelerometer/ControlSystem-Accelerometer/GameScenes/GameScene.swift
@@ -9,10 +9,11 @@ import CoreMotion
 import SpriteKit
 
 class GameScene: SKScene {
-
+    
     private let motionManager = CMMotionManager()
     
     private var spriteNode: SKSpriteNode!
+    private var yOffset: Float = 0
     
     // MARK: - Lifecycle
     override func didMove(to view: SKView) {
@@ -26,15 +27,28 @@ class GameScene: SKScene {
 // MARK: - Accelerometer
 extension GameScene {
     private func setAccelerometer() {
-        if motionManager.isAccelerometerAvailable {
-            motionManager.accelerometerUpdateInterval = 1/60
-            motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { data, error in
-                guard let data = data else { return }
-                let multiplier = Float(data.acceleration.x) * 10
-                let maximumX = (self.view?.bounds.width ?? 0) / 2 - self.spriteNode.size.width / 2
-                let x = self.spriteNode.position.x + CGFloat(multiplier)
-                guard abs(x) < maximumX else { return }
-                self.spriteNode.position = CGPoint(x: x, y: self.spriteNode.position.y)
+        guard motionManager.isAccelerometerAvailable else { return }
+        motionManager.accelerometerUpdateInterval = 1/60
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { data, error in
+            guard let data = data else { return }
+            let additionValueForX = Float(data.acceleration.x) * 10
+            let maximumX = (self.view?.bounds.width ?? 0) / 2 - self.spriteNode.size.width / 2
+            let newX = self.spriteNode.position.x + CGFloat(additionValueForX)
+            
+            if abs(newX) < maximumX {
+                self.spriteNode.position = CGPoint(x: newX, y: self.spriteNode.position.y)
+            }
+            
+            let additionValueForY = Float(data.acceleration.y) * 10
+            if self.yOffset == 0 {
+                self.yOffset = additionValueForY
+            }
+            let maximumY = (self.view?.bounds.height ?? 0) / 2 - self.spriteNode.size.height / 2
+            let newY = self.spriteNode.position.y + CGFloat(additionValueForY) - CGFloat(self.yOffset)
+            print(additionValueForY, CGFloat(additionValueForY) - CGFloat(self.yOffset))
+            
+            if abs(newY) < maximumY {
+                self.spriteNode.position = CGPoint(x: self.spriteNode.position.x, y: newY)
             }
         }
     }
