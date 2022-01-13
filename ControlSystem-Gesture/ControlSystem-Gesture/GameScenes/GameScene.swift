b8e803b7b09ba6539657ded2c36b6d0dfcabb899
KasRoid <KasRoid@gmail.com>
2022-01-13 09:51:07 +0900

    UI: Implemented Animated Texture

--- ControlSystem-Gesture/ControlSystem-Gesture/GameScenes/GameScene.swift
+++ ControlSystem-Gesture/ControlSystem-Gesture/GameScenes/GameScene.swift
@@ -10,14 +10,33 @@ import SpriteKit
 class GameScene: SKScene {
     
     var ship: SKSpriteNode?
+    var movingAnimation: SKAction?
     
+    // MARK: - Lifecycle
     override func didMove(to view: SKView) {
         anchorPoint = CGPoint(x: 0.5, y: 0.5)
         ship = SKSpriteNode(imageNamed: "Ship")
         addChild(ship!)
+        setupAnimation()
     }
     
     override func update(_ currentTime: TimeInterval) {
         
+    }
+}
+
+// MARK: - UI
+extension GameScene {
+    private func setupAnimation() {
+        let atlas = SKTextureAtlas(named: "Ship")
+        var atlasTextures: [SKTexture] = []
+        for number in 1...12 {
+            let name = "Ship\(number)"
+            let texture = atlas.textureNamed(name)
+            atlasTextures.append(texture)
+        }
+        let atlasAnimation = SKAction.animate(with: atlasTextures, timePerFrame: 0.05)
+        movingAnimation = SKAction.repeat(atlasAnimation, count: 20)
+        ship?.run(movingAnimation!)
     }
 }
