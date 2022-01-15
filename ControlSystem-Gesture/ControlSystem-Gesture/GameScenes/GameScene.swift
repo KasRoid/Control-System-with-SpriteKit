75804b279cb26b54835d7ddd6df17209e175be7f
KasRoid <KasRoid@gmail.com>
2022-01-15 22:34:23 +0900

    feat: Rotating and Moving Ship and Target

--- ControlSystem-Gesture/ControlSystem-Gesture/GameScenes/GameScene.swift
+++ ControlSystem-Gesture/ControlSystem-Gesture/GameScenes/GameScene.swift
@@ -10,14 +10,18 @@ import SpriteKit
 class GameScene: SKScene {
     
     private var ship: SKSpriteNode?
+    private var target: SKSpriteNode?
     private var movingAnimation: SKAction?
     private let tapRecognizer = UITapGestureRecognizer()
     private let rotateRecognizer = UIRotationGestureRecognizer()
+    private var offset: CGFloat = 0
+    private let length: CGFloat = 200
+    private var theRotation: CGFloat = 0
     
     // MARK: - Lifecycle
     override func didMove(to view: SKView) {
         anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        addShip()
+        addNodes()
         setupAnimation()
         addGestures(view: view)
     }
@@ -38,22 +42,49 @@ extension GameScene {
 extension GameScene {
     @objc
     func didTap(_ sender: UITapGestureRecognizer) {
+        let fadeAction = SKAction.fadeAlpha(to: 0, duration: 0.5)
+        target?.run(fadeAction)
+        
+        let moveAction = SKAction.move(to: target!.position, duration: 0.6)
+        moveAction.timingMode = .easeOut
+        ship?.run(moveAction)
         ship?.run(movingAnimation!)
     }
     
     @objc
     func didRotate(_ sender: UIRotationGestureRecognizer) {
-        if sender.state == .ended {
-            // TODO
+        switch sender.state {
+        case .began:
+            let fadeAction = SKAction.fadeAlpha(to: 1, duration: 0.5)
+            target?.run(fadeAction)
+        case .changed:
+            theRotation = CGFloat(sender.rotation) + offset
+            theRotation = theRotation * -1
+            ship?.zRotation = theRotation
+            target?.zRotation = theRotation
+            
+            let xDistance: CGFloat = sin(theRotation) * length
+            let yDistance: CGFloat = cos(theRotation) * length
+            target?.position = CGPoint(x: ship!.position.x - xDistance, y: ship!.position.y + yDistance)
+        case .ended:
+            offset = theRotation * -1
+        default:
+            break
         }
     }
 }
 
 // MARK: - UI
 extension GameScene {
-    private func addShip() {
+    private func addNodes() {
         ship = SKSpriteNode(imageNamed: "Ship")
         addChild(ship!)
+        
+        target = SKSpriteNode(imageNamed: "target")
+        guard let x = ship?.position.x, let y = ship?.position.y else { return }
+        target?.position = CGPoint(x: x, y: y + length)
+        target?.alpha = 0
+        addChild(target!)
     }
     
     private func setupAnimation() {
