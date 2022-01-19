c7b734528a7967dac24a5a1f5db0fde79266aad2
KasRoid <KasRoid@gmail.com>
2022-01-19 18:30:44 +0900

    feat: Movements and Speed Based Controls using a Virtual Joystick

--- ControlSystem-VirtualJoystick/ControlSystem-VirtualJoystick/GameScenes/GameScene.swift
+++ ControlSystem-VirtualJoystick/ControlSystem-VirtualJoystick/GameScenes/GameScene.swift
@@ -9,10 +9,16 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    // MARK: - Properties
     private let baseNode = SKSpriteNode(imageNamed: "Base")
     private let ballNode = SKSpriteNode(imageNamed: "Ball")
     private let shipNode = SKSpriteNode(imageNamed: "Ship")
+    
     private var isStickActive = false
+    private var shipSpeedX: CGFloat = 0.1
+    private var shipSpeedY: CGFloat = 0.1
+    private var directionMode = false
+    private var currentDirection: Direction = .N
     
     // MARK: - Lifecycle
     override func didMove(to view: SKView) {
@@ -20,11 +26,15 @@ class GameScene: SKScene {
         addNodes()
     }
     
+    override func update(_ currentTime: TimeInterval) {
+        directionMode ? moveByDirection() : moveByPosition()
+    }
+    
     // MARK: - Touch Events
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            isStickActive = ballNode.frame.contains(location) ? true : false
+            isStickActive = ballNode.frame.contains(location)
         }
     }
     
@@ -34,8 +44,10 @@ class GameScene: SKScene {
             let location = touch.location(in: self)
             let vector = CGVector(dx: location.x - baseNode.position.x, dy: location.y - baseNode.position.y)
             let angle = atan2(vector.dy, vector.dx)
-            let length = baseNode.frame.size.height / 2
+            let degree = angle * CGFloat(180 / Double.pi)
+            currentDirection.setDirection(degree: degree + 180)
             
+            let length = baseNode.frame.size.height / 2
             let radian90: CGFloat = 1.57079633
             let xDistance = sin(angle - radian90) * length
             let yDistance = cos(angle - radian90) * length
@@ -44,6 +56,9 @@ class GameScene: SKScene {
             ? location
             : CGPoint(x: baseNode.position.x - xDistance, y: baseNode.position.y + yDistance)
             shipNode.zRotation = angle - radian90
+            
+            shipSpeedX = vector.dx * 0.1
+            shipSpeedY = vector.dy * 0.1
         }
     }
     
@@ -52,6 +67,42 @@ class GameScene: SKScene {
         let moveAction = SKAction.move(to: baseNode.position, duration: 0.2)
         moveAction.timingMode = .easeOut
         ballNode.run(moveAction)
+    }
+}
+
+// MARK: - Methods
+extension GameScene {
+    private func moveByPosition() {
+        shipNode.position = CGPoint(x: shipNode.position.x + shipSpeedX, y: shipNode.position.y + shipSpeedY)
+    }
+    
+    private func moveByDirection() {
+        var xMove: CGFloat = 0
+        var yMove: CGFloat = 0
+        
+        switch currentDirection {
+        case .N:
+            yMove = 1
+        case .S:
+            yMove = -1
+        case .E:
+            xMove = 1
+        case .W:
+            xMove = -1
+        case .NE:
+            xMove = 1
+            yMove = 1
+        case .NW:
+            xMove = -1
+            yMove = 1
+        case .SE:
+            xMove = 1
+            yMove = -1
+        case .SW:
+            xMove = -1
+            yMove = -1
+        }
+        shipNode.position = CGPoint(x: shipNode.position.x + xMove, y: shipNode.position.y + yMove)
     }
 }
 
@@ -68,6 +119,8 @@ extension GameScene {
         ballNode.position = baseNode.position
         addChild(ballNode)
         shipNode.position = CGPoint(x: 0, y: 200)
+        shipNode.xScale = 0.6
+        shipNode.yScale = 0.6
         addChild(shipNode)
     }
 }
---
+++ ControlSystem-VirtualJoystick/ControlSystem-VirtualJoystick/Models/Direction.swift
@@ -0,0 +1,35 @@
+//
+//  Direction.swift
+//  ControlSystem-VirtualJoystick
+//
+//  Created by Doyoung Song on 1/19/22.
+//
+
+import UIKit
+
+enum Direction: Int {
+    
+    case N, S, E, W, NE, NW, SE, SW
+    
+    mutating func setDirection(degree: CGFloat) {
+        if degree >= 335 {
+            self = .W
+        } else if degree >= 290 {
+            self = .NW
+        } else if degree >= 245 {
+            self = .N
+        } else if degree >= 200 {
+            self = .NE
+        } else if degree >= 155 {
+            self = .E
+        } else if degree >= 110 {
+            self = .SE
+        } else if degree >= 65 {
+            self = .S
+        } else if degree >= 20 {
+            self = .SW
+        } else if degree >= 0 {
+            self = .W
+        }
+    }
+}
