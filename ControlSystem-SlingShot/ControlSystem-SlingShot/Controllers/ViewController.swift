617c4dfd12a835ace5a7bf75c7342db44c6efd09
KasRoid <KasRoid@gmail.com>
2022-01-29 11:08:14 +0900

    init: Control System with Slingshot Style Project

---
+++ ControlSystem-SlingShot/ControlSystem-SlingShot/Controllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ControlSystem-SlingShot
+//
+//  Created by Doyoung Song on 1/29/22.
+//
+
+import SpriteKit
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        view = SKView(frame: UIScreen.main.bounds)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let view = view as? SKView else { return }
+        let scene = SKScene(fileNamed: "GameScene")
+        view.presentScene(scene)
+    }
+}
---
+++ ControlSystem-SlingShot/ControlSystem-SlingShot/Views/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  ControlSystem-SlingShot
+//
+//  Created by Doyoung Song on 1/29/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        print(#function)
+    }
+}
