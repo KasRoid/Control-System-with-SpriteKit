2b81273ff8220f9efa68446f5ffb9ca2437ab80d
KasRoid <KasRoid@gmail.com>
2022-02-01 20:32:57 +0900

    feat: Implemented Physics Body to the Player Node

--- ControlSystem-SlingShot/ControlSystem-SlingShot/Views/GameScene.swift
+++ ControlSystem-SlingShot/ControlSystem-SlingShot/Views/GameScene.swift
@@ -16,17 +16,71 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         setUI()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if triggerNode.contains(location) {
+                
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if triggerNode.contains(location) {
+                let vector = CGVector(dx: location.x - triggerNode.position.x, dy: location.y - triggerNode.position.y)
+                playerNode.position = CGPoint(x: triggerNode.position.x + vector.dx, y: triggerNode.position.y + vector.dy)
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        returnPlayerToTheStartingPoint()
+        addPhysicsToPlayerNode()
+    }
+}
+
+extension GameScene {
+    private func returnPlayerToTheStartingPoint() {
+        let moveAction = SKAction.move(to: triggerNode.position, duration: 0.1)
+        playerNode.run(moveAction)
+    }
+    
+    private func addPhysicsToPlayerNode() {
+        playerNode.physicsBody = SKPhysicsBody(texture: playerNode.texture!, size: playerNode.size)
+        playerNode.physicsBody?.isDynamic = true
+        playerNode.physicsBody?.affectedByGravity = true
+    }
 }
 
 extension GameScene {
     private func setUI() {
         anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        setBackgroundNode()
+        setTriggerNode()
+        setPlayerNode()
+    }
+    
+    private func setBackgroundNode() {
         backgroundNode.size = frame.size
         addChild(backgroundNode)
-        
-        playerNode.position = CGPoint(x: 0, y: 0)
+    }
+    
+    private func setPlayerNode() {
+        playerNode.position = triggerNode.position
+        playerNode.zPosition = 2
         addChild(playerNode)
-        
+    }
+    
+    private func setTriggerNode() {
+        let viewWidth = frame.width
+        let nodeRadius = triggerNode.frame.size.width / 2
+        let offset: CGFloat = 20
+        triggerNode.position = CGPoint(x: -viewWidth / 2 + nodeRadius + offset, y: 0)
+        triggerNode.zPosition = 1
+        triggerNode.alpha = 0.3
         addChild(triggerNode)
     }
 }
